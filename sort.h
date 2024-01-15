fe18d03b15fce52a44f1cd73bc10d37ac2580c84
chloe0524 <chloco31@gmail.com>
2024-01-15 04:46:14 -0800

    fixed issue with betty

--- sort.h
+++ sort.h
@@ -14,9 +14,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /* PROTOTYPES */
