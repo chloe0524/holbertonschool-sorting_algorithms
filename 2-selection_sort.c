0e5975ec4f90521cda7e6c7222eb656dfdf941e0
chloe0524 <chloco31@gmail.com>
2024-01-17 05:30:17 -0800

    betty fixes

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,34 +1,35 @@
 #include "sort.h"
 
-/*
- * selection_sort - Sorts an array of integers in ascending order using Selection sort
+/**
+ * selection_sort - Sorts an array of integers
+ * in ascending order using Selection sort
  * @array: Pointer to the array to be sorted
  * @size: Size of the array
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_index;
-    int temp;
+	size_t i, j, min_index;
+	int temp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_index = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_index])
-                min_index = j;
-        }
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
 
-        if (min_index != i)
-        {
-            temp = array[i];
-            array[i] = array[min_index];
-            array[min_index] = temp;
+		if (min_index != i)
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
 
-            print_array(array, size);
-        }
-    }
+			print_array(array, size);
+		}
+	}
 }
