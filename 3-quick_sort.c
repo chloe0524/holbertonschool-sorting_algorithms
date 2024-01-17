f724da5af58efb3d7046b0c876baaef205cd62fd
tercsam <135607601+tercsam@users.noreply.github.com>
2024-01-17 14:08:58 +0100

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -38,8 +38,9 @@ void quick_sort_recursive(int *array, int low, int high, size_t size) {
 int lomuto_partition(int *array, int low, int high, size_t size) {
     int pivot = array[high];
     int i = low - 1;
+    int j;
 
-    for (int j = low; j <= high - 1; j++) {
+    for (j = low; j <= high - 1; j++) {
         if (array[j] <= pivot) {
             i++;
             swap(&array[i], &array[j]);
