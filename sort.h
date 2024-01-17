b44d1132c837bfa1328d69f2b90d4e9b0a747a39
tercsam <135607601+tercsam@users.noreply.github.com>
2024-01-17 13:19:34 +0100

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,34 @@
+#ifndef SORT_HEADER_H
+#define SORT_HEADER_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* DATA STRUCTURE */
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* PROTOTYPES */
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif /* SORT_HEADER_H */
