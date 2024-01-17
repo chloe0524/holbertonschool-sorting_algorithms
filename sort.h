76806349ecc81def6f05be735300c02b2f73c15b
tercsam <135607601+tercsam@users.noreply.github.com>
2024-01-17 13:58:08 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -30,5 +30,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swap(int *a, int *b);
+int lomuto_partition(int *array, int low, int high, size_t size);
+void quick_sort_recursive(int *array, int low, int high, size_t size);
 
 #endif /* SORT_HEADER_H */
